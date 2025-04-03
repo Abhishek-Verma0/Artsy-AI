37c6bb4908c76cad151b7adb0ff4f1f416b5b61a
Abhishek-Verma0 <neeleshabhishek@gmail.com>
2025-04-04 00:15:08 +0530

    upadted form

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -135,7 +135,7 @@ const chat = model.startChat({
       <form className="newForm" onSubmit={handleSubmit} ref={formRef}>
         <Upload setImg={setImg} />
         <input id="file" type="file" multiple={false} hidden />
-        <input type="text" name="text" placeholder="Ask anything..." />
+        <input type="text" name="text" placeholder="Ask anything..." autoComplete="off" />
         <button>
           <img src="/arrow.png" alt="" />
         </button>
